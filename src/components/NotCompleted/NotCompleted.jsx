685c9814ce96d699de2a4fa7e295e40f68bf36ed
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-21 19:22:34 +0100

    function remove single task from list

--- src/components/NotCompleted/NotCompleted.jsx
+++ src/components/NotCompleted/NotCompleted.jsx
@@ -4,10 +4,12 @@ import classes from './NotCompleted.module.css'
 import { FaCheck } from "react-icons/fa";
 import { BsFillTrashFill } from "react-icons/bs";
 
-const NotCompleted = ({notCompleted}) => {
+const NotCompleted = ({notCompleted, setNotCompleted}) => {
     console.log(notCompleted);
-    const deleteTask = () => {
+    const deleteTask = (id) => {
         console.log('deleted');
+       const newList = notCompleted.filter((item) => item.id !== id);
+       setNotCompleted(newList);
     }
 
     if (notCompleted.length < 1) {
@@ -22,9 +24,9 @@ let todo = null;
  todo = (
   <div className={classes.container} >
       Not Completed
-      {notCompleted.map((task, index) => {
+      {notCompleted.map((task) => {
           return (
-          <div key={index} className={classes.notcompleted} >
+          <div key={task.id} className={classes.notcompleted} >
 
           <h4 >
               {task.text}
@@ -33,7 +35,7 @@ let todo = null;
                 <div className={classes.icons}>
 
                     <FaCheck color='green' />
-                    <BsFillTrashFill onClick={() => deleteTask(index)}/>
+                    <BsFillTrashFill onClick={() => deleteTask(task.id)}/>
                 </div>
           </div> 
           )
